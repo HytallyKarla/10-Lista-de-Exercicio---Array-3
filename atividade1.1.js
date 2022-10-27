b05da3cf75276efc9d95b9c6d66c0a05be3ecb0a
Hytally Karla <hytallylirio@gmail.com>
2022-10-27 20:03:53 -0300

    Lista de Exercicios de ARRAY, para praticar

---
+++ atividade1.1.js
@@ -0,0 +1,25 @@
+/*1. Crie um array para armazenar a idade e outro para armazenar a altura de 10 alunos.
+No final, exiba quantos alunos com mais de 13 anos tem uma altura menor do que a média da altura de todos os alunos.*/
+
+
+var idades = []
+var alturas = []
+
+var quantidadeAlunos = 0
+var mediaAltura = 0 
+
+for (var index = 0; index < 10; index++) {
+    idades[index] = parseInt(prompt("Insira sua idade"))
+    alturas[index] = parseFloat(prompt("Insira sua altura"))
+    mediaAltura = mediaAltura + alturas[index]
+}
+
+mediaAltura = mediaAltura / 10
+
+for (var index = 0; index < 10; index++){
+    if (idades[index] > 13 && alturas[index] < mediaAltura){
+quantidadeAlunos++
+    }
+}
+
+console.log(quantidadeAlunos, " alunos são maiores de 13 anos e possuem a altura menor que a média de alturas.")
---
+++ atividade1.2.js
@@ -0,0 +1,23 @@
+/*2. Faça um algoritmo que receba 10 números inteiros e armazene-os em um array. Depois armazene em um array PAR todos os 
+números que forem pares, e em um array IMPAR os que forem ímpares, no final exiba os três vetores.*/
+
+var array = []
+var arrayPares = []
+var arrayImpares = []
+var indexPares = 0
+var indexImpares = 0
+
+for (var index = 0; index < 10; index++){
+    array[index] = parseInt(prompt("Insira um numero"))
+    if (array[index] % 2 == 0){
+     arrayPares[indexPares] = array[index]
+     indexPares++
+    }else {
+        arrayImpares[indexImpares] = array[index]
+        indexImpares++
+    }
+}
+
+console.log(array)
+console.log(arrayPares)
+console.log(arrayImpares)
